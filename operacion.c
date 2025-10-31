ba45c02a2cb6df463eb5e5d979f30c1bf72c240f
Santiago Leon <Santiagolc0513@gmail.com>
2025-10-31 16:30:36 -0600

    cambios

--- operacion.c
+++ operacion.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
-int multi(int a, int b){
-    return a*b;
+int suma(int a, int b){
+return a+b;
 }
-
+int resta(int a, int b){
+return a-b;
+}
 int main(){
-    int num1 = 12;
-    int num2 = 3;
-
-    int op1 = suma(num1,num2);
-    printf("%i",op1);
-    int op2 = resta(num1,num2);
-    printf("%i",op2);
-    int op3 = multi(num1,num2);
-    printf("%i",op3);
-
-    return 0;
+int num1 = 12;
+int num2 = 3;
+int op1 = suma(num1,num2);
+printf("%i",op1);
+int op2 = resta(num1,num2);
+printf("%i",op2);
+int op3 = multi(num1,num2);
+printf("%i",op3);
+return 0;
 }
-
 
